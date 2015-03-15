5ae099e2a648f4452a334a3ef5fa30d5654306ce
Richelle29 <chua.richelle@yahoo.com>
2015-03-16 02:13:59 +0800

    Update CreditMemoController.java

--- src/CreditMemo/CreditMemoController.java
+++ src/CreditMemo/CreditMemoController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package CreditMemo;
 
 import Purchases.PurchaseTransactionController;
@@ -12,11 +7,8 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.table.TableModel;
 
-/**
- *
- * @author Janine
- */
-public class CreditMemoController {
+public class CreditMemoController 
+{
     private CreditMemoModel creditmemoModel;
     private CreditMemoListGUI gui;
     private int itemcount;
