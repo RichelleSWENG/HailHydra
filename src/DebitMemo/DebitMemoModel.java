d9fe78d094637b710fe4756a9d50ef2cdfd5bef3
Richelle29 <chua.richelle@yahoo.com>
2015-03-16 01:08:24 +0800

    Update DebitMemoModel.java

--- src/DebitMemo/DebitMemoModel.java
+++ src/DebitMemo/DebitMemoModel.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package DebitMemo;
 
 import Database.DBConnection;
@@ -12,10 +7,6 @@ import java.sql.Statement;
 import javax.swing.table.TableModel;
 import net.proteanit.sql.DbUtils;
 
-/**
- *
- * @author Janine
- */
 public class DebitMemoModel {
     protected Connection db;
     protected Statement statement;
