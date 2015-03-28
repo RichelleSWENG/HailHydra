dc27cee8262abf475810a8d00e78e3590146d857
janinetan14 <janine_tan10@yahoo.com>
2015-03-29 01:03:12 +0800

    changed all floats to decimal

--- March29.sql
+++ March29.sql
@@ -53,15 +53,15 @@ CREATE TABLE `acknowledgementreceipt` (
   `acknowledgement_receipt_id` varchar(45) NOT NULL,
   `company_id` int(11) NOT NULL,
   `date` date NOT NULL,
-  `original_amount` float NOT NULL DEFAULT '0',
+  `original_amount` decimal(10,2) NOT NULL DEFAULT '0.00',
   `po_num` varchar(45) DEFAULT NULL,
   `ordered_by` varchar(45) DEFAULT NULL,
   `sales_person` varchar(45) DEFAULT NULL,
   `delivered_by` varchar(45) DEFAULT NULL,
   `delivery_notes` varchar(45) DEFAULT NULL,
   `delivery_receipt_num` varchar(45) DEFAULT NULL,
-  `discount` float DEFAULT NULL,
-  `current_balance` float NOT NULL DEFAULT '0',
+  `discount` decimal(10,2) DEFAULT NULL,
+  `current_balance` decimal(10,2) NOT NULL DEFAULT '0.00',
   `status` varchar(45) NOT NULL,
   `address` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`acknowledgement_receipt_id`),
@@ -76,7 +76,7 @@ CREATE TABLE `acknowledgementreceipt` (
 
 LOCK TABLES `acknowledgementreceipt` WRITE;
 /*!40000 ALTER TABLE `acknowledgementreceipt` DISABLE KEYS */;
-INSERT INTO `acknowledgementreceipt` VALUES ('1234',2,'2014-02-12',10,'123','123','123','123','123','123',123,123,'Open',''),('6',1,'2015-03-23',290,'6','a','a','a','a','6',0,290,'Open',NULL);
+INSERT INTO `acknowledgementreceipt` VALUES ('1234',2,'2014-02-12',10.00,'123','123','123','123','123','123',123.00,123.00,'Open',''),('23',2,'2015-03-29',87.00,'345','','','','','45',12.00,87.00,'Open',NULL),('6',1,'2015-03-23',290.00,'6','a','a','a','a','6',0.00,290.00,'Open',NULL);
 /*!40000 ALTER TABLE `acknowledgementreceipt` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -91,8 +91,8 @@ CREATE TABLE `arlineitem` (
   `acknowledgement_receipt_id` varchar(45) NOT NULL,
   `quantity` int(11) NOT NULL,
   `part_num` varchar(20) NOT NULL,
-  `unit_price` float NOT NULL,
-  `line_total` float NOT NULL,
+  `unit_price` decimal(10,2) NOT NULL,
+  `line_total` decimal(10,2) NOT NULL,
   KEY `acknowledgement_receipt_id_idx` (`acknowledgement_receipt_id`),
   KEY `part_num_idx` (`part_num`),
   CONSTRAINT `acknowledgement_receipt_id` FOREIGN KEY (`acknowledgement_receipt_id`) REFERENCES `acknowledgementreceipt` (`acknowledgement_receipt_id`) ON UPDATE CASCADE,
@@ -106,7 +106,7 @@ CREATE TABLE `arlineitem` (
 
 LOCK TABLES `arlineitem` WRITE;
 /*!40000 ALTER TABLE `arlineitem` DISABLE KEYS */;
-INSERT INTO `arlineitem` VALUES ('1234',2,'123',10,20),('6',1,'234',70,70),('6',1,'SHV_E300K',220,220);
+INSERT INTO `arlineitem` VALUES ('1234',2,'123',10.00,20.00),('6',1,'234',70.00,70.00),('6',1,'SHV_E300K',220.00,220.00),('23',1,'345',87.00,87.00);
 /*!40000 ALTER TABLE `arlineitem` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -120,7 +120,7 @@ DROP TABLE IF EXISTS `collection`;
 CREATE TABLE `collection` (
   `collection_id` int(11) NOT NULL AUTO_INCREMENT,
   `date` date NOT NULL,
-  `amount` float NOT NULL,
+  `amount` decimal(10,2) NOT NULL,
   `receipt_type` varchar(45) NOT NULL,
   `number` varchar(45) NOT NULL,
   `notes` varchar(500) DEFAULT NULL,
@@ -162,7 +162,7 @@ CREATE TABLE `company` (
   `email` varchar(45) DEFAULT NULL,
   `contact_person` varchar(45) DEFAULT NULL,
   `status` varchar(8) NOT NULL DEFAULT 'Active',
-  `credit_limit` float NOT NULL,
+  `credit_limit` decimal(10,2) NOT NULL,
   `terms` int(11) NOT NULL,
   `type` varchar(45) NOT NULL,
   PRIMARY KEY (`company_id`)
@@ -175,7 +175,7 @@ CREATE TABLE `company` (
 
 LOCK TABLES `company` WRITE;
 /*!40000 ALTER TABLE `company` DISABLE KEYS */;
-INSERT INTO `company` VALUES (1,'Janine','asdsaffdgdsas','ffsdsadasd','fasdfsafdf','afddsafasdfaf','123','123','123','123','dsfdsfsd','dfsdf','sdfsdfsfsfsd','Active',12,3,'Retail Customer'),(2,'Richelle','adkldjsfsk','sdfjslkdfjl','ksdflskdjfl','kdflkjflkdjsa','123','123','123','123','kxdlksjl','kjsdlkfjsl','kdfslkj','Active',12,3,'Walk-in Customer'),(4,'dsfdfsdfs','dsfsdfsfsdf23232','fdsfsdfsdf23232','fdsfsdf23232','1234','21834344','','','2313421','','fdgfhfggsdfsfds','','Active',20,3,'Retail Customer'),(5,'janien','','','','','','','','','','','','Active',30000,30,'Sister Company Customer'),(6,'adsad','','','','','','','','','','','','Active',20,30,'Sister Company Customer'),(7,'jeje','taft ','Manila','','','','','','','','','','Active',30,30,'Sister Company Customer'),(8,'bianca','','','','','','','','','','','','Active',100,30,'Supplier');
+INSERT INTO `company` VALUES (1,'Janine','asdsaffdgdsas','ffsdsadasd','fasdfsafdf','afddsafasdfaf','123','123','123','123','dsfdsfsd','dfsdf','sdfsdfsfsfsd','Active',12.00,3,'Retail Customer'),(2,'Richelle','adkldjsfsk','sdfjslkdfjl','ksdflskdjfl','kdflkjflkdjsa','123','123','123','123','kxdlksjl','kjsdlkfjsl','kdfslkj','Active',12.00,3,'Walk-in Customer'),(4,'dsfdfsdfs','dsfsdfsfsdf23232','fdsfsdfsdf23232','fdsfsdf23232','1234','21834344','','','2313421','','fdgfhfggsdfsfds','','Active',20.00,3,'Retail Customer'),(5,'janien','','','','','','','','','','','','Active',30000.00,30,'Sister Company Customer'),(6,'adsad','','','','','','','','','','','','Active',20.00,30,'Sister Company Customer'),(7,'jeje','taft ','Manila','','','','','','','','','','Active',30.00,30,'Sister Company Customer'),(8,'bianca','','','','','','','','','','','','Active',100.00,30,'Supplier');
 /*!40000 ALTER TABLE `company` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -220,7 +220,7 @@ CREATE TABLE `debitmemo` (
   `debit_memo_id` varchar(45) NOT NULL,
   `date` date NOT NULL,
   `company_id` int(11) NOT NULL,
-  `total_amount` float NOT NULL,
+  `total_amount` decimal(10,2) NOT NULL,
   `receipt_type` varchar(45) NOT NULL,
   `number` varchar(45) NOT NULL,
   `approved_by` varchar(45) DEFAULT NULL,
@@ -239,7 +239,7 @@ CREATE TABLE `debitmemo` (
 
 LOCK TABLES `debitmemo` WRITE;
 /*!40000 ALTER TABLE `debitmemo` DISABLE KEYS */;
-INSERT INTO `debitmemo` VALUES ('1','2014-02-26',1,20,'Acknowledgement Receipt','1','sdsad','sdasda','2014-02-27','sadasddfdss',0);
+INSERT INTO `debitmemo` VALUES ('1','2014-02-26',1,20.00,'Acknowledgement Receipt','1','sdsad','sdasda','2014-02-27','sadasddfdss',0);
 /*!40000 ALTER TABLE `debitmemo` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -254,8 +254,8 @@ CREATE TABLE `dmlineitem` (
   `debit_memo_id` varchar(45) NOT NULL,
   `quantity` int(11) NOT NULL,
   `part_num` varchar(20) NOT NULL,
-  `unit_price` float NOT NULL,
-  `line_total` float NOT NULL,
+  `unit_price` decimal(10,2) NOT NULL,
+  `line_total` decimal(10,2) NOT NULL,
   KEY `part_num_idx` (`part_num`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -266,7 +266,7 @@ CREATE TABLE `dmlineitem` (
 
 LOCK TABLES `dmlineitem` WRITE;
 /*!40000 ALTER TABLE `dmlineitem` DISABLE KEYS */;
-INSERT INTO `dmlineitem` VALUES ('1',2,'123',10,20);
+INSERT INTO `dmlineitem` VALUES ('1',2,'123',10.00,20.00);
 /*!40000 ALTER TABLE `dmlineitem` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -282,10 +282,10 @@ CREATE TABLE `item` (
   `description` varchar(100) NOT NULL,
   `stock_minimum` int(11) NOT NULL,
   `rack_location` varchar(45) NOT NULL,
-  `last_cost` float NOT NULL DEFAULT '0',
-  `walk_in_price` float NOT NULL DEFAULT '0',
-  `traders_price` float NOT NULL DEFAULT '0',
-  `sister_company_price` float NOT NULL DEFAULT '0',
+  `last_cost` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `walk_in_price` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `traders_price` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `sister_company_price` decimal(10,2) NOT NULL DEFAULT '0.00',
   `quantity_defective` int(11) NOT NULL DEFAULT '0',
   `quantity_functional` int(11) NOT NULL DEFAULT '0',
   `notes` varchar(500) DEFAULT NULL,
@@ -301,7 +301,7 @@ CREATE TABLE `item` (
 
 LOCK TABLES `item` WRITE;
 /*!40000 ALTER TABLE `item` DISABLE KEYS */;
-INSERT INTO `item` VALUES ('123','nothing',2,'sdda',60,50,50,50,0,0,'','C:/Users/Janine/Desktop/Inventory/Screen Shot 2015-03-16 at 11.36.15 AM.png',1),('234','cdfjdsl',2,'czxcm,dc',40,60,70,80,1,1,'','C:/Users/Janine/Desktop/Inventory/Screen Shot 2015-03-14 at 10.18.37 PM.png',1),('345','great description',5,'',156,87,78,30,0,0,'','C:/Users/Janine/Desktop/Inventory/Screen Shot 2015-03-16 at 11.35.54 AM.png',1),('SHV_E300K','Samsung S4',3,'Rack Number 5',200,210,220,230,1,5,NULL,'C:/Users/Janine/Desktop/Inventory/Screen Shot 2015-03-14 at 10.32.56 PM.png',1);
+INSERT INTO `item` VALUES ('123','nothing',2,'sdda',60.00,50.00,50.00,50.00,0,0,'','C:/Users/Janine/Desktop/Inventory/Screen Shot 2015-03-16 at 11.36.15 AM.png',1),('234','cdfjdsl',2,'czxcm,dc',40.00,60.00,70.00,80.00,1,1,'','C:/Users/Janine/Desktop/Inventory/Screen Shot 2015-03-14 at 10.18.37 PM.png',1),('345','great description',5,'',156.00,87.00,78.00,30.00,0,0,'','C:/Users/Janine/Desktop/Inventory/Screen Shot 2015-03-16 at 11.35.54 AM.png',1),('SHV_E300K','Samsung S4',3,'Rack Number 5',200.00,210.00,220.00,230.00,1,5,NULL,'C:/Users/Janine/Desktop/Inventory/Screen Shot 2015-03-14 at 10.32.56 PM.png',1);
 /*!40000 ALTER TABLE `item` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -314,7 +314,7 @@ DROP TABLE IF EXISTS `payments`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `payments` (
   `payment_id` int(11) NOT NULL AUTO_INCREMENT,
-  `amount` float NOT NULL,
+  `amount` decimal(10,2) NOT NULL,
   `purchase_transaction_id` int(11) NOT NULL,
   `notes` varchar(500) DEFAULT NULL,
   `date` date NOT NULL,
@@ -350,7 +350,7 @@ CREATE TABLE `ptlineitem` (
   `quantity` int(11) NOT NULL,
   `part_num` varchar(20) NOT NULL,
   `unit_price` int(11) NOT NULL,
-  `line_total` float NOT NULL,
+  `line_total` decimal(10,2) NOT NULL,
   KEY `purchase_transaction_id_idx` (`purchase_transaction_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -361,7 +361,7 @@ CREATE TABLE `ptlineitem` (
 
 LOCK TABLES `ptlineitem` WRITE;
 /*!40000 ALTER TABLE `ptlineitem` DISABLE KEYS */;
-INSERT INTO `ptlineitem` VALUES (1,2,'abc',12,24),(2,2,'abc',12,24);
+INSERT INTO `ptlineitem` VALUES (1,2,'abc',12,24.00),(2,2,'abc',12,24.00);
 /*!40000 ALTER TABLE `ptlineitem` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -376,19 +376,19 @@ CREATE TABLE `purchasetransaction` (
   `purchase_transaction_id` int(11) NOT NULL AUTO_INCREMENT,
   `company_id` int(11) NOT NULL,
   `date` date NOT NULL,
-  `original_amount` float NOT NULL,
-  `discount` float DEFAULT NULL,
+  `original_amount` decimal(10,2) NOT NULL,
+  `discount` decimal(10,2) DEFAULT NULL,
   `ref_sales_invoice_num` varchar(45) DEFAULT NULL,
   `ordered_by` varchar(45) DEFAULT NULL,
   `po_num` varchar(45) DEFAULT NULL,
   `received_by` varchar(45) DEFAULT NULL,
   `receiving_notes` varchar(45) DEFAULT NULL,
-  `vat` float DEFAULT NULL,
+  `vat` decimal(10,2) DEFAULT NULL,
   `delivery_receipt_num` varchar(45) DEFAULT NULL,
-  `current_balance` float NOT NULL,
+  `current_balance` decimal(10,2) NOT NULL,
   `status` varchar(45) NOT NULL,
   `address` varchar(100) DEFAULT NULL,
-  `subtotal` float DEFAULT '0',
+  `subtotal` decimal(10,2) DEFAULT '0.00',
   PRIMARY KEY (`purchase_transaction_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -399,7 +399,7 @@ CREATE TABLE `purchasetransaction` (
 
 LOCK TABLES `purchasetransaction` WRITE;
 /*!40000 ALTER TABLE `purchasetransaction` DISABLE KEYS */;
-INSERT INTO `purchasetransaction` VALUES (1,2,'2015-01-13',23,1,'fsdfs','dfs','fsf','sdfdsfs','dsfsfs',12,'ddfdsv',12,'Open','sdfdgdss',0),(2,1,'2016-02-02',123,1,'dfdsf','dfsdf','dfsfs','dsfs','sdfsdf',13,'cvsdf',12,'Closed','sdsadsasd',0),(3,1,'2015-01-01',12,1,'dfsd','dsfsf','dfdsfs','dsfds','fsdf',14,'fsdf',12,'Closed','fdfsds',0);
+INSERT INTO `purchasetransaction` VALUES (1,8,'2015-01-13',23.00,1.00,'fsdfs','dfs','fsf','sdfdsfs','dsfsfs',12.00,'ddfdsv',12.00,'Open','sdfdgdss',0.00),(2,8,'2016-02-02',123.00,1.00,'dfdsf','dfsdf','dfsfs','dsfs','sdfsdf',13.00,'cvsdf',12.00,'Closed','sdsadsasd',0.00),(3,8,'2015-01-01',12.00,1.00,'dfsd','dsfsf','dfdsfs','dsfds','fsdf',14.00,'fsdf',12.00,'Closed','fdfsds',0.00);
 /*!40000 ALTER TABLE `purchasetransaction` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -413,7 +413,7 @@ DROP TABLE IF EXISTS `returnslip`;
 CREATE TABLE `returnslip` (
   `return_slip_id` varchar(45) NOT NULL,
   `date` date NOT NULL,
-  `total_amount` float NOT NULL,
+  `total_amount` decimal(10,2) NOT NULL,
   `company_id` int(11) NOT NULL,
   `purchase_transaction_num` int(11) NOT NULL,
   `returned_by` varchar(45) DEFAULT NULL,
@@ -435,7 +435,7 @@ CREATE TABLE `returnslip` (
 
 LOCK TABLES `returnslip` WRITE;
 /*!40000 ALTER TABLE `returnslip` DISABLE KEYS */;
-INSERT INTO `returnslip` VALUES ('1','2014-02-26',20,1,1,'cxzcxz','2014-03-12','xczczxx','xczxxcz','xzczczxc',NULL,NULL,NULL);
+INSERT INTO `returnslip` VALUES ('1','2014-02-26',20.00,8,1,'cxzcxz','2014-03-12','xczczxx','xczxxcz','xzczczxc',NULL,NULL,NULL),('120','2015-03-29',0.00,8,2,'','2015-03-29','','',NULL,'Functional','2015-03-29','2015-03-29'),('125','2015-03-29',20.00,8,2,'','2015-03-29','','',NULL,'Functional','2015-03-29','2015-03-29'),('129','2015-03-29',200.00,8,1,'','2015-03-29','','',NULL,'Functional','2015-03-29','2015-03-29'),('23','2015-03-29',20.00,8,1,'','2015-03-29','','',NULL,'Functional','2015-03-29','2015-03-29');
 /*!40000 ALTER TABLE `returnslip` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -450,8 +450,8 @@ CREATE TABLE `rslineitem` (
   `return_slip_id` varchar(45) NOT NULL,
   `quantity` int(11) NOT NULL,
   `part_num` varchar(20) NOT NULL,
-  `unit_price` float NOT NULL,
-  `line_total` float NOT NULL,
+  `unit_price` decimal(10,2) NOT NULL,
+  `line_total` decimal(10,2) NOT NULL,
   `status` int(1) NOT NULL DEFAULT '0',
   KEY `return_slip_id_idx` (`return_slip_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -463,7 +463,7 @@ CREATE TABLE `rslineitem` (
 
 LOCK TABLES `rslineitem` WRITE;
 /*!40000 ALTER TABLE `rslineitem` DISABLE KEYS */;
-INSERT INTO `rslineitem` VALUES ('1',2,'123',10,20,0);
+INSERT INTO `rslineitem` VALUES ('1',2,'123',10.00,20.00,0),('23',1,'345',20.00,20.00,0),('125',1,'234',20.00,20.00,0),('120',1,'234',0.00,0.00,0),('129',1,'234',200.00,200.00,0);
 /*!40000 ALTER TABLE `rslineitem` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -478,9 +478,9 @@ CREATE TABLE `salesinvoice` (
   `sales_invoice_id` varchar(45) NOT NULL,
   `company_id` int(11) NOT NULL,
   `date` date NOT NULL,
-  `original_amount` float NOT NULL,
-  `vat` float NOT NULL,
-  `discount` float NOT NULL,
+  `original_amount` decimal(10,2) NOT NULL,
+  `vat` decimal(10,2) NOT NULL,
+  `discount` decimal(10,2) NOT NULL,
   `po_num` varchar(45) DEFAULT NULL,
   `ordered_by` varchar(45) DEFAULT NULL,
   `sales_person` varchar(45) DEFAULT NULL,
@@ -488,10 +488,10 @@ CREATE TABLE `salesinvoice` (
   `delivery_notes` varchar(45) DEFAULT NULL,
   `delivery_receipt_num` varchar(45) DEFAULT NULL,
   `pwd_id_number_notes` varchar(45) DEFAULT NULL,
-  `current_balance` float NOT NULL DEFAULT '0',
+  `current_balance` decimal(10,2) NOT NULL DEFAULT '0.00',
   `status` varchar(45) NOT NULL DEFAULT 'Open',
   `address` varchar(100) DEFAULT NULL,
-  `subtotal` float NOT NULL DEFAULT '0',
+  `subtotal` decimal(10,2) NOT NULL DEFAULT '0.00',
   PRIMARY KEY (`sales_invoice_id`),
   KEY `customerid_idx` (`company_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -503,7 +503,7 @@ CREATE TABLE `salesinvoice` (
 
 LOCK TABLES `salesinvoice` WRITE;
 /*!40000 ALTER TABLE `salesinvoice` DISABLE KEYS */;
-INSERT INTO `salesinvoice` VALUES ('1234',2,'2017-02-12',21,212,12,'12','21','sdasd','dada','sadasda','123','xcvcsc',12,'Open','',0);
+INSERT INTO `salesinvoice` VALUES ('1234',2,'2017-02-12',21.00,212.00,12.00,'12','21','sdasd','dada','sadasda','123','xcvcsc',12.00,'Open','',0.00);
 /*!40000 ALTER TABLE `salesinvoice` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -518,8 +518,8 @@ CREATE TABLE `silineitem` (
   `sales_invoice_id` varchar(45) NOT NULL,
   `quantity` int(11) NOT NULL,
   `part_num` varchar(20) NOT NULL,
-  `unit_price` float NOT NULL DEFAULT '0',
-  `line_total` float NOT NULL DEFAULT '0',
+  `unit_price` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `line_total` decimal(10,2) NOT NULL DEFAULT '0.00',
   KEY `sales_invoice_id_idx` (`sales_invoice_id`),
   CONSTRAINT `sales_invoice_id` FOREIGN KEY (`sales_invoice_id`) REFERENCES `salesinvoice` (`sales_invoice_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -531,7 +531,7 @@ CREATE TABLE `silineitem` (
 
 LOCK TABLES `silineitem` WRITE;
 /*!40000 ALTER TABLE `silineitem` DISABLE KEYS */;
-INSERT INTO `silineitem` VALUES ('1234',2,'123',10,20);
+INSERT INTO `silineitem` VALUES ('1234',2,'123',10.00,20.00);
 /*!40000 ALTER TABLE `silineitem` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -571,4 +571,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-03-28 23:46:50
+-- Dump completed on 2015-03-29  1:01:58
