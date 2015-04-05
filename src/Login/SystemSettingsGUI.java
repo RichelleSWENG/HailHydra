9c807dac8e28c4d1a875ce1ed5e01a59f8ac4e5a
Richelle29 <chua.richelle@yahoo.com>
2015-04-05 13:36:58 +0800

    Update SystemSettingsGUI.java

--- src/Login/SystemSettingsGUI.java
+++ src/Login/SystemSettingsGUI.java
@@ -17,7 +17,8 @@ public class SystemSettingsGUI extends JPanel {
 	private Font fntPlainText;
 	private GUIController controller;
 
-	public SystemSettingsGUI(GUIController temp) {
+	public SystemSettingsGUI(GUIController temp) 
+        {
 		controller=temp;
                 
 		setBounds(5, 5, 580, 390);
@@ -29,18 +30,34 @@ public class SystemSettingsGUI extends JPanel {
 		btnModifyPassword.setFont(fntPlainText);
 		btnModifyPassword.setBounds(130, 50, 330, 40);
 		add(btnModifyPassword);
+                btnModifyPassword.addActionListener(new ActionListener() 
+                {
+			public void actionPerformed(ActionEvent e) 
+                        {
+				controller.changePanelToModifyPassword();
+			}
+		});
 
 		btnModifySystemProfile = new JButton("Modify System Profile");
 		btnModifySystemProfile.setFont(fntPlainText);
 		btnModifySystemProfile.setBounds(130, 110, 330, 40);
 		add(btnModifySystemProfile);
+                btnModifySystemProfile.addActionListener(new ActionListener() 
+                {
+			public void actionPerformed(ActionEvent e) 
+                        {
+				controller.changePanelToModifySystemProfile();
+			}
+		});
 
 		btnModifyVAT = new JButton("Modify VAT");
 		btnModifyVAT.setFont(fntPlainText);
 		btnModifyVAT.setBounds(130, 170, 330, 40);
 		add(btnModifyVAT);
-		btnModifyVAT.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+		btnModifyVAT.addActionListener(new ActionListener() 
+                {
+			public void actionPerformed(ActionEvent e) 
+                        {
 				controller.getAlert("VAT");
 			}
 		});
@@ -49,8 +66,10 @@ public class SystemSettingsGUI extends JPanel {
 		btnModifyCreditLimitAlert.setFont(fntPlainText);
 		btnModifyCreditLimitAlert.setBounds(130, 230, 330, 40);
 		add(btnModifyCreditLimitAlert);
-		btnModifyCreditLimitAlert.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+		btnModifyCreditLimitAlert.addActionListener(new ActionListener() 
+                {
+			public void actionPerformed(ActionEvent e) 
+                        {
 				controller.getAlert("CreditLimit");
 			}
 		});
@@ -59,8 +78,10 @@ public class SystemSettingsGUI extends JPanel {
 		btnModifyTermsAlert.setFont(fntPlainText);
 		btnModifyTermsAlert.setBounds(130, 290, 330, 40);
 		add(btnModifyTermsAlert);
-		btnModifyTermsAlert.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+		btnModifyTermsAlert.addActionListener(new ActionListener() 
+                {
+			public void actionPerformed(ActionEvent e) 
+                        {
 				controller.getAlert("Terms");
 			}
 		});
