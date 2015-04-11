5fdd9a4a5b12ec7a71a53b202b92d7051cf8ddf5
Richelle29 <chua.richelle@yahoo.com>
2015-04-11 18:43:53 +0800

    Update HailHydraGUI.java

--- src/HailHydra/HailHydraGUI.java
+++ src/HailHydra/HailHydraGUI.java
@@ -1,22 +1,30 @@
 package HailHydra;
 
 
+import Database.DBConnection;
 import Database.DatabaseGUI;
 
 import java.awt.BorderLayout;
 import java.awt.SystemColor;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 
 public class HailHydraGUI extends JFrame
 {
+    private Connection db;
     private JPanel pnlContent, pnlDatabase;
     
     
     public HailHydraGUI()
     {
                 setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                setTitle();
                 this.setResizable(false);
 		setBounds(100, 100, 1000, 650);
                 setVisible(true);
@@ -29,7 +37,28 @@ public class HailHydraGUI extends JFrame
                 pnlDatabase=new DatabaseGUI();
                 getContentPane().add(pnlDatabase, BorderLayout.SOUTH);
               
-  
+                
+                
+    }
+    
+    public void setTitle()
+    {
+        DBConnection dbc = new DBConnection();
+                db=dbc.getConnection();
+                try
+                {
+                Statement statement= db.createStatement();
+                String sql="SELECT company_name FROM systeminfo WHERE system_info_id=1 ";
+                ResultSet rs= statement.executeQuery(sql);
+                ResultSetMetaData metadata = rs.getMetaData();
+                if(rs.next())
+                {
+                this.setTitle(""+rs.getString(1));
+                }
+                }catch(Exception e)
+                {
+                    e.printStackTrace();
+                }
     }
     
     public JPanel getContentPanel()
