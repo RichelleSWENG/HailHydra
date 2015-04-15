f472e79a2c477bf84f9b210bd8591fca74a5474f
Richelle29 <chua.richelle@yahoo.com>
2015-04-16 00:19:00 +0800

    Update AddAccountProfileGUI.java

--- src/AccountProfile/AddAccountProfileGUI.java
+++ src/AccountProfile/AddAccountProfileGUI.java
@@ -2,7 +2,6 @@ package AccountProfile;
 
 import HailHydra.GUIController;
 
-import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
@@ -40,160 +39,140 @@ public class AddAccountProfileGUI extends AccountProfileGUI
                     {
                         boolean error = false;
 
-                        if (tfName.getText().equals("") || ftfCreditLimit.getText().equals("") || ftfTerms.getText().equals("")) 
-                        {
-                            JOptionPane.showMessageDialog(null, "Please fill in the required fields");
-                            error = true;
-                        }
-
-                        if (tfName.getText().length() > 50) 
-                        {
-                            JOptionPane.showMessageDialog(null, "Name can not exceed 50 characters. Please re-input the name.");
-                            error = true;
-                            lblName.setForeground(Color.RED);
-                        }
-
-                        if (taAddress.getText().length() > 100) 
-                        {
-                            JOptionPane.showMessageDialog(null, "Address can not exceed 100 characters. Please re-input the address.");
-                            error = true;
-                            lblAddress.setForeground(Color.RED);
-                        }
-
-                        if (tfCity.getText().length() > 45)
-                        {
-                            JOptionPane.showMessageDialog(null, "City can not exceed 45 characters. Please re-input the city.");
-                            error = true;
-                            lblCity.setForeground(Color.RED);
-                        }
-
-                        if (tfCountry.getText().length() > 45)
-                        {
-                            JOptionPane.showMessageDialog(null, "Country can not exceed 45 characters. Please re-input the country.");
-                            error = true;
-                            lblCountry.setForeground(Color.RED);
-                        }
-
-                        if (tfPostCode.getText().length() > 45)
-                        {
-                            JOptionPane.showMessageDialog(null, "Postcode can not exceed 45 characters. Please re-input the postcode.");
-                            error = true;
-                            lblPostCode.setForeground(Color.RED);
-                        }
-
-                        if (tfPhone1.getText().length() > 45) 
-                        {
-                            JOptionPane.showMessageDialog(null, "Phone #1 can not exceed 11 characters. Please re-input the phone #1.");
-                            error = true;
-                            lblPhone1.setForeground(Color.RED);
-                        }
-
-                        if (tfPhone2.getText().length() > 45) 
-                        {
-                            JOptionPane.showMessageDialog(null, "Phone #2 can not exceed 11 characters. Please re-input the phone #2.");
-                            error = true;
-                            lblPhone2.setForeground(Color.RED);
-                        }
-
-                        if (tfPhone3.getText().length() > 45) 
-                        {
-                            JOptionPane.showMessageDialog(null, "Phone #3 can not exceed 11 characters. Please re-input the phone #3.");
-                            error = true;
-                            lblPhone3.setForeground(Color.RED);
-                        }
-
-                        if (tfFaxNumber.getText().length() > 45) 
-                        {
-                            JOptionPane.showMessageDialog(null, "Fax Number can not exceed 11 characters. Please re-input the city.");
-                            error = true;
-                            lblFaxNumber.setForeground(Color.RED);
-                        }
-
-                        if (tfEmailAddress.getText().length() > 45) 
-                        {
-                            JOptionPane.showMessageDialog(null, "Email address can not exceed 45 characters. Please re-input the email.");
-                            error = true;
-                            lblEmailAddress.setForeground(Color.RED);
-                        }
-
-                        if (tfWebsite.getText().length() > 45) 
-                        {
-                            JOptionPane.showMessageDialog(null, "Website can not exceed 45 characters. Please re-input the website.");
-                            error = true;
-                            lblWebsite.setForeground(Color.RED);
-                        }
-
-                        if (tfContactPerson.getText().length() > 45) 
-                        {
-                            JOptionPane.showMessageDialog(null, "Contact person can not exceed 45 characters. Please re-input the contact person.");
-                            error = true;
-                            lblContactPerson.setForeground(Color.RED);
-                        }
-
-                        if (ftfCreditLimit.getText().length() > 12)
-                        {
-                            JOptionPane.showMessageDialog(null, "Credit Limit can not exceed 999,999,999,999.");
-                            error = true;
-                            lblCreditLimit.setForeground(Color.RED);
-                        }
-
-                        if (Float.parseFloat(ftfCreditLimit.getText().replaceAll(",", "")) < 0.00f) 
-                        {
-                            JOptionPane.showMessageDialog(null, "Credit Limit is invalid.");
-                            error = true;
-                        }
-                        
-                        if(Integer.parseInt(ftfTerms.getText().replaceAll(",", ""))<0|| Integer.parseInt(ftfTerms.getText().replaceAll(",", ""))>2000)
-                        {
-                        JOptionPane.showMessageDialog(null, "Terms(Days) is invalid");
-                            error = true;
-                        }
-
-                        if (!"".equals(tfPhone1.getText())) 
-                        {
-                            if (!isInteger(tfPhone1.getText()) || Integer.parseInt(tfPhone1.getText()) < 0) 
+                        if(tfName.getText().equals("") || ftfCreditLimit.getText().equals("") || ftfTerms.getText().equals(""))
                             {
-                                JOptionPane.showMessageDialog(null, "Tel Phone 1 # is invalid");
+                                JOptionPane.showMessageDialog(null, "Please fill-up all required fields.");
                                 error = true;
                             }
-                        }
-
-                        if (!"".equals(tfPhone2.getText())) 
-                        {
-                            if (!isInteger(tfPhone2.getText()) || Integer.parseInt(tfPhone2.getText()) < 0) 
-                            {
-                                JOptionPane.showMessageDialog(null, "Tel Phone 2 # is invalid");
+                            
+                            if(tfName.getText().length()>50)
+                            {                           
+                                JOptionPane.showMessageDialog(null, "Name can not exceed 50 characters. Please re-input the name.");
                                 error = true;
                             }
-                        }
-
-                        if (!"".equals(tfPhone3.getText())) 
-                        {
-                            if (!isInteger(tfPhone3.getText()) || Integer.parseInt(tfPhone3.getText()) < 0)
+                            
+                            if(taAddress.getText().length()>100)
                             {
-                                JOptionPane.showMessageDialog(null, "Phone#3 is invalid.");
+                                JOptionPane.showMessageDialog(null, "Address can not exceed 100 characters. Please re-input the address.");
                                 error = true;
                             }
-                        }
-
-                        if (!"".equals(tfFaxNumber.getText())) 
-                        {
-                            if (!isInteger(tfFaxNumber.getText()) || Integer.parseInt(tfFaxNumber.getText()) < 0)
+                            
+                            if(tfCity.getText().length()>45)
                             {
-                                JOptionPane.showMessageDialog(null, "Fax Number is invalid.");
+                                JOptionPane.showMessageDialog(null, "City can not exceed 45 characters. Please re-input the city.");
                                 error = true;
                             }
-                        }
+                            
+                            if(tfCountry.getText().length()>45)
+                            {
+                                JOptionPane.showMessageDialog(null, "Country can not exceed 45 characters. Please re-input the country.");
+                                error = true;
+                            }
+                            
+                            if(tfPostCode.getText().length()>45)
+                            {
+                                JOptionPane.showMessageDialog(null, "Postcode can not exceed 45 characters. Please re-input the postcode.");
+                                error = true;
+                            }
+                            
+                            if(Integer.parseInt(ftfTerms.getText().replaceAll(",", ""))>2000)
+                            {
+                                JOptionPane.showMessageDialog(null, "Terms can not be greater than 2,000. Please re-input the terms.");
+                                error = true;
+                            }
+                                
+                            if(tfPhone1.getText().length()>45)
+                            {
+                                JOptionPane.showMessageDialog(null, "Phone #1 can not exceed 11 characters. Please re-input the phone #1.");
+                                error = true;
+                            }
+                            
+                            if(tfPhone2.getText().length()>45)
+                            {
+                                JOptionPane.showMessageDialog(null, "Phone #2 can not exceed 11 characters. Please re-input the phone #2.");
+                                error = true;
+                            }
+                            
+                            if(tfPhone3.getText().length()>45)
+                            {
+                                JOptionPane.showMessageDialog(null, "Phone #3 can not exceed 11 characters. Please re-input the phone #3.");
+                                error = true;
+                            }
+                            
+                            if(tfFaxNumber.getText().length()>45)
+                            {
+                                JOptionPane.showMessageDialog(null, "Fax Number can not exceed 11 characters. Please re-input fax number.");
+                                error = true;
+                            }
+                            
+                            if(tfEmailAddress.getText().length()>45)
+                            {
+                                JOptionPane.showMessageDialog(null, "Email address can not exceed 45 characters. Please re-input email.");
+                                error = true;
+                            }
+                            
+                            if(tfWebsite.getText().length()>45)
+                            {
+                                JOptionPane.showMessageDialog(null, "Website can not exceed 45 characters. Please re-input website.");
+                                error = true;
+                            }
+                            
+                            if(tfContactPerson.getText().length()>45)
+                            {
+                                JOptionPane.showMessageDialog(null, "Contact person can not exceed 45 characters. Please re-input contact person.");
+                                error = true;
+                            }
+                            
+                            if(ftfCreditLimit.getText().length()>12)
+                            {
+                                JOptionPane.showMessageDialog(null, "Credit Limit can not exceed 9,999,999");
+                                error = true;
+                            }  
 
-                        if (hasSpecial(tfPostCode.getText())) 
-                        {
-                            JOptionPane.showMessageDialog(null, "Please enter a valid postal code.");
-                            error = true;
-                        }
+                            if(Float.parseFloat(ftfCreditLimit.getText().replaceAll(",", ""))<0.00f)
+                            {
+                                JOptionPane.showMessageDialog(null, "Credit Limit is invalid.");
+                                error= true;
+                            }
+                            
+                            if(!"".equals(tfPhone1.getText()))
+                                if(!isInteger(tfPhone1.getText()) || Integer.parseInt(tfPhone1.getText())<0)
+                                {
+                                    JOptionPane.showMessageDialog(null, "Phone#1  is invalid.");
+                                    error = true;
+                                }
+                            
+                            if(!"".equals(tfPhone2.getText()))
+                                if(!isInteger(tfPhone2.getText()) || Integer.parseInt(tfPhone2.getText())<0)
+                                {
+                                    JOptionPane.showMessageDialog(null, "Phone#2 is invalid.");
+                                    error = true;
+                                }
+                            
+                            if(!tfPhone3.getText().isEmpty())
+                                if(!isInteger(tfPhone3.getText()) || Integer.parseInt(tfPhone3.getText())<0)
+                                {
+                                    JOptionPane.showMessageDialog(null, "Phone#3 is invalid.");
+                                    error = true;
+                                }
+                            
+                            if(!"".equals(tfFaxNumber.getText())) 
+                                if(!isInteger(tfFaxNumber.getText()) || Integer.parseInt(tfFaxNumber.getText())<0)
+                                {
+                                    JOptionPane.showMessageDialog(null, "Fax Number is invalid.");
+                                    error = true;
+                                }
+                     
+                            if(hasSpecial(tfPostCode.getText()))
+                            {
+                                JOptionPane.showMessageDialog(null, "Please enter a valid postal code.");
+                                error = true;
+                            }      
 
                         if (error == false) 
                         {
-                            try{
+                            try
+                            {
                                 al.removeAll(al);
                                 
                                 al.add(tfName.getText().toString());
