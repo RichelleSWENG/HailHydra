aeb21deb49726a5d8e8826356ba3ebecb88ca792
janinetan14 <janine_tan10@yahoo.com>
2015-04-17 20:21:23 +0800

    Fixed Format Database

--- src/Database/DatabaseGUI.java
+++ src/Database/DatabaseGUI.java
@@ -8,30 +8,29 @@ import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 import javax.swing.border.BevelBorder;
 
+public class DatabaseGUI extends JPanel
+{
+	private JLabel lblStatus;
+	private boolean connectionStatus;
+	private Font fntDatabase;
 
-public class DatabaseGUI extends JPanel{
-    private JLabel lblStatus;
-    private boolean connectionStatus;
-    private Font fntDatabase;
-    
-    public DatabaseGUI(){
-                fntDatabase=new Font("Arial", Font.PLAIN, 14);
+	public DatabaseGUI()
+	{
+		fntDatabase = new Font("Arial", Font.PLAIN, 14);
 		setBorder(new BevelBorder(BevelBorder.LOWERED));
 		setPreferredSize(new Dimension(this.getWidth(), 20));
 		lblStatus = new JLabel("");
-                lblStatus.setFont(fntDatabase);
+		lblStatus.setFont(fntDatabase);
 		lblStatus.setHorizontalAlignment(SwingConstants.LEFT);
-                setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+		setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
 		add(lblStatus);
- 
-                
-                
-                DBConnection dbc = new DBConnection();
+
+		DBConnection dbc = new DBConnection();
 		this.connectionStatus = dbc.getConnectionStatus();
 		if (connectionStatus)
 			lblStatus.setText("Database Connected.");
 		else
 			lblStatus.setText("Database Not Connected.");
-    }
-    
+	}
+
 }
