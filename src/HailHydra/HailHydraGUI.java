a6c816176e6ec299a86f1acc86362401dc4783f2
janinetan14 <janine_tan10@yahoo.com>
2015-04-17 20:23:59 +0800

    Fixed Format HailHydra

--- src/HailHydra/HailHydraGUI.java
+++ src/HailHydra/HailHydraGUI.java
@@ -1,6 +1,5 @@
 package HailHydra;
 
-
 import Database.DBConnection;
 import Database.DatabaseGUI;
 import ModifyAlert.FactoryModify;
@@ -20,60 +19,60 @@ import java.awt.Toolkit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-
 public class HailHydraGUI extends JFrame
 {
-    private Connection db;
-    private JPanel pnlContent, pnlDatabase;
-    
-    
-    public HailHydraGUI(DBConnection db)
-    {
-                this.db=db.getConnection();
-                setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                setTitle();
-                //setIconImage(Toolkit.getDefaultToolkit().createImage("hailhydra.PNG"));
-                this.setResizable(false);
+	private Connection db;
+	private JPanel pnlContent, pnlDatabase;
+
+	public HailHydraGUI(DBConnection db)
+	{
+		this.db = db.getConnection();
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle();
+		// setIconImage(Toolkit.getDefaultToolkit().createImage("hailhydra.PNG"));
+		this.setResizable(false);
 		setBounds(100, 100, 1000, 650);
-                setVisible(true);
-                
+		setVisible(true);
+
 		pnlContent = new JPanel();
 		pnlContent.setLayout(null);
 		getContentPane().add(pnlContent);
 		getContentPane().add(pnlContent);
-                
-                pnlDatabase=new DatabaseGUI();
-                getContentPane().add(pnlDatabase, BorderLayout.SOUTH);
-    }
-    
-    public void setTitle()
-    {
-        ResultSet rs = null;
-        try
-        {
-            Statement statement = db.createStatement();
-            String sql = "SELECT company_name FROM systeminfo WHERE system_info_id='1'";
-            rs = statement.executeQuery(sql);
 
-        } catch (Exception e)
-        {
-            e.getMessage();
-        }
-        try {
-                if(!rs.next())
-                {
-                    this.setTitle("");
-                    return;
-                }
-                else
-                    this.setTitle(rs.getString("company_name"));
-            } catch (SQLException ex) {
-                Logger.getLogger(FactoryModify.class.getName()).log(Level.SEVERE, null, ex);
-            }
-    }
-    
-    public JPanel getContentPanel()
-    {
-        return pnlContent;
-    }
+		pnlDatabase = new DatabaseGUI();
+		getContentPane().add(pnlDatabase, BorderLayout.SOUTH);
+	}
+
+	public void setTitle()
+	{
+		ResultSet rs = null;
+		try
+		{
+			Statement statement = db.createStatement();
+			String sql = "SELECT company_name FROM systeminfo WHERE system_info_id='1'";
+			rs = statement.executeQuery(sql);
+
+		} catch (Exception e)
+		{
+			e.getMessage();
+		}
+		try
+		{
+			if (!rs.next())
+			{
+				this.setTitle("");
+				return;
+			} else
+				this.setTitle(rs.getString("company_name"));
+		} catch (SQLException ex)
+		{
+			Logger.getLogger(FactoryModify.class.getName()).log(Level.SEVERE,
+					null, ex);
+		}
+	}
+
+	public JPanel getContentPanel()
+	{
+		return pnlContent;
+	}
 }
