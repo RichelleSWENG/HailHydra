ba0d0764ad54b0996062506bbd05b5814231dbca
Richelle29 <chua.richelle@yahoo.com>
2015-04-19 19:03:47 +0800

    Update ARLineItemModel.java

--- src/AcknowledgementReceipt/ARLineItemModel.java
+++ src/AcknowledgementReceipt/ARLineItemModel.java
@@ -2,20 +2,9 @@ package AcknowledgementReceipt;
 
 import java.sql.ResultSet;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.sql.PreparedStatement;
 import java.sql.Connection;
 import Database.DBConnection;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author Jolo Simeon
- */
 public class ARLineItemModel
 {
 	protected Connection db;
